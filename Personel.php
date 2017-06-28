89e689c662884160899d37bf6da10fbd3b32c547
Nuh <hex-tr@hotmail.com>
2017-06-28 16:08:44 +0300

    deneme

---
+++ Personel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nuh
+ * Date: 28.06.2017
+ * Time: 16:06
+ */
+
+namespace Personel;
+
+
+class Personel
+{
+
+    public $id;
+    public $adi;
+    public $soyadi;
+
+}
+
